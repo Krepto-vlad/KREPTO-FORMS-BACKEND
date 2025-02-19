d114246ed74ff2d8599442d7f2261126bd8629c6
Vlad <vladshokoladkin@gmail.com>
2025-02-19 18:56:06 +0200

    test port

--- src/server.ts
+++ src/server.ts
@@ -6,14 +6,9 @@ import authRoutes from "./routes/authRoutes";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRoutes);
