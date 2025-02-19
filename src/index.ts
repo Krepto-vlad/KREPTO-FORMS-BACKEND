84131a593e40b0e400f3e2b05ac108ba04bf6752
Vlad <vladshokoladkin@gmail.com>
2025-02-19 19:00:42 +0200

    moved to index.ts

--- src/index.ts
+++ src/index.ts
@@ -51,3 +51,22 @@
 // app.listen(port, () => {
 //   console.log(`Сервер работает на порту ${port}`);
 // });
+
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
