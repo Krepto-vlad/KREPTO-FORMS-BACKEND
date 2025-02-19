04c750fb4e563dbbb1a35d21a8bf4125a1dd4b5f
Vlad <vladshokoladkin@gmail.com>
2025-02-19 20:38:40 +0200

    fixed cors

--- src/index.ts
+++ src/index.ts
@@ -50,7 +50,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+      cors({
+        origin: process.env.CLIENT_URL || "http://localhost:5173",
+        credentials: true,
+      })
+);
 app.use(express.json());
 
 app.use("/auth", authRoutes);
@@ -60,7 +65,7 @@ app.listen(PORT, () => {
 });
 
 
-app.get("/test", async (req, res) => {
+app.get("/test-db", async (req, res) => {
     try {
       
       return res.status(200).json({
